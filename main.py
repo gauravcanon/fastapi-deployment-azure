8740adc463b4058334f2570257f397878de21251
Gaurav Dubey <gauravdubey@shiftboolean.com>
2023-10-05 10:36:08 +0530

    Added startup command for unvicorn

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from typing import Union
-
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+# Add uvicorn start command
 
 
 @app.get("/")
@@ -14,7 +17,18 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.get("/users/me")
 def read_user_me():
     # generate a fake username for demo purposes
     return {"username": "fakecurrentuser"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app,
+                host="0.0.0.0",
+                port=8000,
+                proxy_headers=True,
+                forwarded_allow_ips="*",
+                )
+
